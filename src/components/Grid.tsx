89b6708c1cd95675c5f033064bc8e6ebdb36d87b
Tim Charteris <tim.charterii@gmail.com>
2025-01-08 19:07:43 +0000

    leave for evening, needs updates to state handling of grid for ease of access

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { initializeGrid } from "./utils/Grid";
 import './css/Grid.css';
   
@@ -11,7 +11,21 @@ export default function SudokuGrid() {
         togglingSums: false,
         selectedCell: null,
     });
+    
+    // maybe update grid state using all helper functions
+    // seperate state from grid and update grid with helper functions !!!
 
+    useEffect(() => {
+        console.log(state);
+    }, [state]);
+
+    const handleCellChange = (e, cell) => {
+        cell.value = e.target.value;
+    }
+    
+    const handleCellClick = (cell) => {
+        
+    }
     return (
         // This is the grid that will be rendered. It will be updated by the state object.
         // All components need to be able to access the state object that will edit this grid.
@@ -23,7 +37,8 @@ export default function SudokuGrid() {
                         <div
                             key={cell.id}
                             className={`cell ${cell.isSelected ? 'selected' : ''} ${cell.isIncorrect ? 'incorrect' : ''}`}
-                            // onClick={() => handleCellClick(cell)}
+                            onClick={() => handleCellClick(cell)}
+                            onChange={(e) => handleCellChange(e, cell)}
                         >
                             <input 
                                 type="text"
