b36456185213e4e4a97f50b74c7874c8c5c1f81b
Tim Charteris <tim.charterii@gmail.com>
2025-01-09 13:31:58 +0000

    create state setter functions for use across the app

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,57 @@ function App() {
   const [grid, setGrid] = useState(initializeGrid()); // Sets the grid initially ready for logic to handle its data
   const [state, setState] = useState(initialState) // Sets the state of the app to the initial state 
 
+  // State update functions using the spread operator to update grid
+  // ADD FUNCTIONS HERE LATER
+  // NOT REALLY SURE WHAT THESE LOOK LIKE YET
+
+  // State update functions using spread operator to update state
+
+  // Function to set the isValid state
+  const setIsValid = (value: boolean) => {
+    setState(prevState => ({
+      ...prevState,
+      isValid: value,
+    }));
+  };
+
+  // Function to set the deletingBox state
+  const setDeletingBox = (value: boolean) => {
+    setState(prevState => ({
+      ...prevState,
+      deletingBox: value,
+    }));
+  };
+
+  // Function to set the settingBoxTotal
+  const setSettingBoxTotal = (value: boolean) => {
+    setState(prevState => ({
+      ...prevState,
+      settingBoxTotal: value,
+    }));
+  };
+
+  // Function to set the togglingSums state
+  const setTogglingSums = (value: boolean) => {
+    setState(prevState => ({
+      ...prevState,
+      togglingSums: value,
+    }));
+  };
+
+  // Function to set the killerMode state
+  const setKillerMode = (value: boolean) => {
+    setState(prevState => ({
+      ...prevState,
+      killerMode: value,
+    }));
+  };
+
+    // Not 100% sure what to do with this yet
+  const handleSelectedCell = (value: any) => {
+    // fill in later
+  };
+
   // For debugging will delete later
   useEffect(() => {
     console.log(state);
--- src/app-utils/initialState.ts
+++ src/app-utils/initialState.ts
@@ -8,3 +8,4 @@ const initialState = {
 }
 
 export default initialState
+
