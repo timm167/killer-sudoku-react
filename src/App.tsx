861dc9006be07f5c1a168f875eb583fa6c177e1c
Tim Charteris <tim.charterii@gmail.com>
2025-01-09 09:25:57 +0000

    handle grid and state in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React, {useState} from 'react'
-import './App.css'
+import React, {useEffect, useState} from 'react'
+import { initializeGrid } from "./components/utils/Grid";
 import SudokuGrid from './components/Grid'
+import initialState from './app-utils/initialState'
+import './App.css' 
 
 function App() {
+  const [grid, setGrid] = useState(initializeGrid()); 
+  const [state, setState] = useState(initialState)  
+
+  useEffect(() => {
+    console.log(state);
+    console.log(grid);
+  }, [state, grid])
 
   return (
     <>
@@ -10,7 +19,7 @@ function App() {
         <h1>Killer Sudoku</h1>
       </div>
       <div>
-        <SudokuGrid/>
+        <SudokuGrid grid={grid} state={state}/>
       </div>
     </>
   )
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,22 +1,11 @@
 import React, {useEffect, useState} from "react";
-import { initializeGrid } from "./utils/Grid";
 import './css/Grid.css';
   
-export default function SudokuGrid() {
-    const [state, setState] = useState({ // State should move to app.tsx
-        grid: initializeGrid(),
-        isValid: true,
-        deletingBox: false,
-        settingBoxTotal: false,
-        togglingSums: false,
-        selectedCell: null,
-    });
-    
-    // maybe update grid state using all helper functions
-    // seperate state from grid and update grid with helper functions !!!
+export default function SudokuGrid({grid, state}) {
 
     useEffect(() => {
         console.log(state);
+        console.log(grid);
     }, [state]);
 
     const handleCellChange = (e, cell) => {
@@ -27,11 +16,9 @@ export default function SudokuGrid() {
         
     }
     return (
-        // This is the grid that will be rendered. It will be updated by the state object.
-        // All components need to be able to access the state object that will edit this grid.
         // maps through the grid, then each row, to define a div for each cell (I need this for adding extra non-input elements later)
         <div className="grid">
-            {state.grid.map((row, rowIndex) => (
+            {grid.map((row, rowIndex) => (
                 <div key={rowIndex} className="row">
                     {row.map((cell, cellIndex) => (
                         <div
